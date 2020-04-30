b6f6127bf992bb7e86d02857276790e537b2de35
alejandro baranek <alejandrobaranek@gmail.com>
2020-04-30 11:02:57 -0300

    comment

--- R/retriever-lib.R
+++ R/retriever-lib.R
@@ -17,11 +17,12 @@ retrieveURL <- function(url, dest.dir = getEnv("data_dir"), force = FALSE){
 retrieveArgentinasDeathsStatistics <- function(){
  data.dir <- getEnv("data_dir")
  #http://www.deis.msal.gov.ar/index.php/base-de-datos/
+ # TODO link scrape automation with rvest
  #deaths.stats.html <- read_html("http://www.deis.msal.gov.ar/index.php/base-de-datos/")
  #deaths.stats.html %>% html_nodes("") %>% html_attr('href')
  #deaths.stats.html %>% html_attr('href')
 
- retrieveURL("http://www.deis.msal.gov.ar/wp-content/uploads/2019/01/DescDef1.xlsx")
+ retrieveURL(url = "http://www.deis.msal.gov.ar/wp-content/uploads/2019/01/DescDef1.xlsx")
  retrieveURL(url = "http://www.deis.msal.gov.ar/wp-content/uploads/2020/01/DefWeb18.csv")
  retrieveURL(url = "http://www.deis.msal.gov.ar/wp-content/uploads/2019/01/DefWeb17.csv")
  retrieveURL(url = "http://www.deis.msal.gov.ar/wp-content/uploads/2018/06/DefWeb16.csv")
